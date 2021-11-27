db35a306c693aee4499963e329533435df9ecc75
manisha-ui <65649884+manisha-ui@users.noreply.github.com>
2021-11-27 23:39:03 +0530

    Update forward_proxy.py

--- forward_proxy.py
+++ forward_proxy.py
@@ -231,3 +231,9 @@ urls = ["google.com",
 
 server = Server(dd, urls)
 
+#To run : python3 forward_proxy.py 
+# Clear  and disable the cache of the chrome
+# Change the proxy settings 
+# Turn the proxy server on and bind it to the localhost and a port
+# [Settings>>Change Proxy settings]
+# Open your browser and open a website (HTTP). Example: http://barc.gov.in, http://example.com
